b05b9ee6c150a335bdcc56e3cbf31850f5651ce3
DarioCoppolecchia <dariocoppolecchia01@gmail.com>
2022-09-21 19:58:43 +0200

    modified discretizer

--- scripts/ConnectionsClassifier/DiscretizerModule/Discretizer.py
+++ scripts/ConnectionsClassifier/DiscretizerModule/Discretizer.py
@@ -1,46 +1,135 @@
 import abc
-from typing import Any # to create an abstract class
 
 class Discretizer(metaclass=abc.ABCMeta):
-    def __init__(self) -> None:
+    """Abstract class that contains the methods to:
+    * create the intervals as a list of floats. 
+    * discretize one value (get the bin where that value is included in the bounds)
+
+    The list of floats is structured in this way:
+    Given a set of intervals where:
+    * let n the number of interval
+    * the list has n + 1 elements
+    * let i, 0 <= i <= n:
+    * li is the lower bound of the i-nth interval
+    * ui is the upper bound of the i-nth interval
+
+    the list will be like this
+    [ l_1, u_1/l_2, u_2/l_3, ... , u_(n-1)/l_n, u_n]
+    where u_(i-1)/l_i means that the element is both the upper bound of
+    the (i-1)-nth interval and the lower bound of the i-nth element
+
+    :param discretized_bins: list that contains the intervals after calling discretize
+    :type discretized_bins: list
+    """
+    def __init__(self, n_bins: int=None) -> None:
+        """Constructor that initialize discretized_bins with and empty list
+
+        :param n_bins: number of bins, defaults to None
+        :type n_bins: int, optional
+        """
         self.discretized_bins = []
+        self.n_bins = n_bins
 
     @abc.abstractmethod
-    def discretize(self, values: list):
+    def discretize(self, values: list) -> list:
+        """Analizes the list of values in input to create the bins
+
+        :param values: list of values to discretize
+        :type values: list
+        :return: the discretized list
+        :rtype: list
+        """
         pass
 
     @abc.abstractmethod
-    def discretize_attribute(self, value: float):
+    def discretize_attribute(self, value: float) -> tuple:
+        """Given a value such that: min(discretized_bins) <= value <= max(discretized_bins), 
+        this returns the bin associated to that value
+
+        :param value: value to discretize
+        :type value: float
+        :return: the return dipends on the value in input
+        * if the value is between lower bound and upper bound, this returns the
+        bounds as a tuple (lower bound, upper bound)
+        * else return a tuple with (0, 0)
+        :rtype: tuple
+        """
         pass
 
 class Equal_Height_Discretizer(Discretizer):
+    """
+    Class that discretize with the Equal height algorithm a list of values and 
+    returns the bin relative to a particulare value
+    """    
 
-    def __init__(self, values: list):
-        self.values = values
+    def __init__(self, n_bins: int=None):
+        """
+        Constructor of this class, calls the super constructor
 
-    def discretize_attribute(self, value: float):
+        :param n_bins: number of bins, defaults to None
+        :type n_bins: int, optional
+        """
+        super().__init__(n_bins)
+
+    def discretize(self, values: list) -> list:
+        """Analizes the list of values in input to create the bins
+
+        :param values: list of values to discretize with equal height
+        :type values: list
+        :return: the discretized list
+        :rtype: list
+        """
+        pass
+
+    def discretize_attribute(self, value: float) -> tuple:
+        """Given a value such that: min(discretized_bins) <= value <= max(discretized_bins), 
+        this returns the bin associated to that value using the equal height algorithm
+
+        :param value: value to discretize
+        :type value: float
+        :return: the return dipends on the value in input
+        * if the value is between lower bound and upper bound, this returns the
+        bounds as a tuple (lower bound, upper bound)
+        * else return a tuple with (0, 0)
+        :rtype: tuple
+        """
         pass
 
 class Equal_Width_Discretizer(Discretizer):
+    """
+    Class that discretize with the Equal width algorithm a list of values and 
+    returns the bin relative to a particulare value
+    """
 
-    def __init__(self, min: float, max: float, n_bins: int) -> None:
-        super().__init__()
-        self.min = min
-        self.max = max
-        self.n_bins = n_bins
+    def __init__(self, n_bins: int=None) -> None:
+        """Constructor of this class, calls the super constructor
+        and initializes the parameters
 
-    def discretize_attribute(self, value: float):
+        :param n_bins: number of bins , defaults to None
+        :type n_bins: int, optional
+        """
+        super().__init__(n_bins)
+
+    def discretize(self, values: list) -> list:
+        """Analizes the list of values in input to create the bins
+
+        :param values: list of values to discretize with equal width
+        :type values: list
+        :return: the discretized list
+        :rtype: list
+        """
         pass
-    
-    def set_min(self, min: float):
-        self.min = min
-    def set_max(self, max: float):
-        self.max = max
-    def set_n_bin(self, n_bin: int):
-        self.n_bin = n_bin
-    def get_min(self) -> float:
-        return self.min
-    def get_max(self) -> float:
-        return self.max
-    def get_n_bins(self) -> int:
-        return self.n_bins
+
+    def discretize_attribute(self, value: float) -> tuple:
+        """Given a value such that: min(discretized_bins) <= value <= max(discretized_bins), 
+        this returns the bin associated to that value using the equal height algorithm
+
+        :param value: value to discretize
+        :type value: float
+        :return: the return dipends on the value in input
+        * if the value is between lower bound and upper bound, this returns the
+        bounds as a tuple (lower bound, upper bound)
+        * else return a tuple with (0, 0)
+        :rtype: tuple
+        """
+        pass
