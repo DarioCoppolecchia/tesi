a58bc0c5f651b56f112a1e66459e7440afff7f17
DarioCoppolecchia <dariocoppolecchia01@gmail.com>
2022-11-07 17:35:00 +0100

    Fixata creazione dataset

--- scripts/ConnectionsAnomalyDetector/MachineLearning/PetriNet.py
+++ scripts/ConnectionsAnomalyDetector/MachineLearning/PetriNet.py
@@ -3,15 +3,16 @@ import pm4py
 from pm4py.algo.discovery.inductive import algorithm as ind_miner
 from pm4py.algo.discovery.inductive.variants.im_f.algorithm import Parameters
 from pm4py.objects.conversion.process_tree import converter
+from pm4py.objects.log.obj import EventLog
 
 class PetriNet:
-    def train(self, xes_log: pm4py.objects.log.obj.EventLog, delta: float, activity: str) -> None:
+    def train(self, xes_log: EventLog, delta: float, activity: str) -> None:
         ptree = ind_miner.apply_tree(xes_log, parameters={Parameters.NOISE_THRESHOLD: delta, Parameters.ACTIVITY_KEY: activity}, variant=ind_miner.Variants.IMf)
         self.__model = converter.apply(ptree)
 
-    def calc_conformance(self, xes_log: pm4py.objects.log.obj.EventLog) -> DataFrame:
+    def calc_conformance(self, xes_log: EventLog) -> DataFrame:
         if type(self.__model) == tuple: # check if the model has been created
-            return pm4py.fitness_alignments(xes_log, self.__model[0], self.__model[1], self.__model[2], multi_processing=True)
+            return pm4py.fitness_alignments(xes_log, self.__model[0], self.__model[1], self.__model[2])
         raise TypeError('The petri net has not been discovered yet, or is not a tuple')
 
     def save_model(self, file_name: str) -> None:
--- scripts/ConnectionsAnomalyDetector/MachineLearning/PetriNetCollector.py
+++ scripts/ConnectionsAnomalyDetector/MachineLearning/PetriNetCollector.py
@@ -1,21 +1,41 @@
 from pandas import DataFrame
 from .PetriNet import PetriNet
 import pm4py
+from pm4py.objects.log.obj import EventLog
+from pm4py.objects.log.obj import Trace
+from pm4py.objects.log.obj import Event
+import tqdm
 
 class PetriNetCollector:
     def __init__(self, attrs: list, delta: float) -> None:
         self.__dict_petriNet = {}
         self.__delta = delta
         for attr in attrs:
-            self.__dict_petriNet[attr] = PetriNet()
+            self.__dict_petriNet[f'concept:{attr}'] = PetriNet()
     
     def load_xes(self, file_name: str) -> None:
         self.__data_log = pm4py.read_xes(file_name)
 
     def train(self) -> None:
         for attr, pn in self.__dict_petriNet.items():
-            pn.train(self.__data_log, self.__delta, f'concept:{attr}')
+            pn.train(self.__data_log, self.__delta, attr)
 
     def create_PetriNet_dataset(self) -> DataFrame:
-        print(list(self.__dict_petriNet.values())[0].calc_conformance(self.__data_log))
-        #return DataFrame([pn.calc_conformance(self.__data_log) for pn in self.__dict_petriNet.values()])
+        df = DataFrame()
+        res = [0 for _ in self.__dict_petriNet.values()]
+        for i, (attr, pn) in enumerate(tqdm.tqdm(self.__dict_petriNet.items())):
+            for trace in self.__data_log:
+                trace_temp = Trace()
+                #trace_temp.attributes = copy.deepcopy(trace.attributes)
+                for event in trace:
+                    event_temp = Event()
+                    for key in event:
+                        if key != attr:
+                            event_temp[key] = event[key]
+                        else:
+                            event_temp['Activity'] = event[key]
+                    trace_temp.append(event_temp)
+                log = EventLog([trace_temp])
+                res[i] = pn.calc_conformance(log)['average_trace_fitness']
+            df[attr] = res
+        return df
