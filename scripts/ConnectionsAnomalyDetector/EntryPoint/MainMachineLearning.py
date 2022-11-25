436a7384f21f18adcea3c2d4d0a5b7918a0e6272
DarioCoppolecchia <dariocoppolecchia01@gmail.com>
2022-11-25 20:27:05 +0100

    Aggiornata pre-consegna file

--- scripts/ConnectionsAnomalyDetector/EntryPoint/MainMachineLearning.py
+++ scripts/ConnectionsAnomalyDetector/EntryPoint/MainMachineLearning.py
@@ -70,6 +70,7 @@ class MainMachineLearning:
         if 'Files' in config:
             path_of_petriNet_models_dataset_test = config['Files']['path_of_petriNet_models_dataset_test'] if 'path_of_petriNet_models_dataset_test' in config['Files'] else '../models/PetriNets/dataset_test/pn'
             path_of_file_xes_test = config['Files']['path_of_file_xes_test'] if 'path_of_file_xes_test' in config['Files'] else '../logs/ML/conn_test.xes'
+
         else:
             path_of_petriNet_models_dataset_test = '../models/PetriNets/dataset_test/pn'
             path_of_file_xes_test = '../logs/ML/conn_test.xes'
@@ -80,6 +81,7 @@ class MainMachineLearning:
         dataset, Y = self.__petriNetCollector.create_PetriNet_dataset(path_of_petriNet_models_dataset_test)
         dataset['label'] = Y
         dataset.to_csv('../models/PetriNets/dataset_test.csv')
+        self.__gen_csv_pred_fittizia(dataset, Y, '../models/PetriNets/report.csv')
         #print('predicting with Anomaly Detector...')
         #y_pred = self.__anomalyDetector.predict(dataset)
         #for i in range(1, len(dataset.columns) + 1):
@@ -87,7 +89,41 @@ class MainMachineLearning:
         #    print(f'printing the results conf matrix {i}...')
         #    self.__anomalyDetector.create_confusion_matrix(Y, y_pred, i)
 
-    def predizione(self, X: pd.DataFrame, soglia: int):
+    def __getDFRow(self, soglia, crDict, cm):
+        row = {}
+        row['soglia'] = soglia
+        row['TP'] = cm[1, 1]
+        row['FN'] = cm[1, 0]
+        row['FP'] = cm[0, 1]
+        row['TN'] = cm[0, 0]
+        row['precision (positive)'] = crDict['1']['precision']
+        row['recall (positive)'] = crDict['1']['recall']
+        row['fscore (positive)'] = crDict['1']['f1-score']
+        row['precision (negative)'] = crDict['-1']['precision']
+        row['recall (negative)'] = crDict['-1']['recall']
+        row['fscore (negative)'] = crDict['-1']['f1-score']
+        row['accuracy'] = crDict['accuracy']
+        row['macroF'] = crDict['macro avg']['f1-score']
+        row['WeigthedF'] = crDict['weighted avg']['f1-score']
+        return row
+    
+    def __gen_csv_pred_fittizia(self, X, Y, file_name_out):
+        import matplotlib.pyplot as plt
+        from tqdm import tqdm
+        from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay
+
+        report = []
+        for i in tqdm(range(1, len(X.columns) + 1)):
+            y_pred = self.__prediction(X, i)
+            cr = classification_report(Y, y_pred, output_dict=True)
+            cm = confusion_matrix(Y, y_pred)
+            cm_display = ConfusionMatrixDisplay(confusion_matrix = cm)
+            cm_display.plot()
+            plt.show()
+            report.append(self.__getDFRow(i, cr, cm))
+        pd.DataFrame(report).to_csv(file_name_out)
+
+    def __prediction(self, X: pd.DataFrame, soglia: int):
         y_pred = []
         for _, x in X.iterrows():
             if sum([1 if attr != 1 else 0 for attr in x]) >= soglia:
